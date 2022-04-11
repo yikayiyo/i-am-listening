a8ef38814d443e1ec6de11836713182f41668b81
yikayiyo <837937787@qq.com>
2022-04-11 23:59:42 +0800

    feat: support episode

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -5,29 +5,43 @@ export default async function handler(req, res) {
   if (response.status === 204 || response.status > 400) {
     return res.status(200).json({ isPlaying: false });
   }
-
-  const song = await response.json();
-  if (song.item === null) {
+  const data = await response.json();
+  if (data.item === null) {
     return res.status(200).json({ isPlaying: false });
   }
-  const isPlaying = song.is_playing;
-  const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-  const album = song.item.album.name;
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
 
+  let isPlaying = false;
+  let title = "";
+  let artist = "";
+  let belongTo = "";
+  let imageUrl = "";
+  let itemUrl = "";
+  // data type
   res.setHeader(
     'Cache-Control',
     'public, s-maxage=60, stale-while-revalidate=30'
   );
-
+  if (data.currently_playing_type === 'episode') {
+    isPlaying = data.is_playing;
+    title = data.item.name;
+    artist = data.item.show.publisher;
+    belongTo = data.item.show.name;
+    imageUrl = data.item.show.images[0].url;
+    itemUrl = data.item.external_urls.spotify;
+  } else {
+    isPlaying = data.is_playing;
+    title = data.item.name;
+    artist = data.item.artists.map((_artist) => _artist.name).join(', ');
+    belongTo = data.item.album.name;
+    imageUrl = data.item.album.images[0].url;
+    itemUrl = data.item.external_urls.spotify;
+  }
   return res.status(200).json({
-    album,
-    albumImageUrl,
+    album: belongTo,
+    albumImageUrl: imageUrl,
     artist,
     isPlaying,
-    songUrl,
+    songUrl: itemUrl,
     title
   });
 }
