ee4ac907cf9ff013f7ab627058b0c8611dea635c
yikayiyo <837937787@qq.com>
2022-04-11 11:46:34 +0800

    try again

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
     <div>
       <Head>
         <title>I'm listening to</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="You can know what I'm listening to here" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
